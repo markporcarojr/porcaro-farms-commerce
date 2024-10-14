fb040fb56a33927ba4c8c74e42f5e99ba236bd9f
Mark <markporcarojr@gmail.com>
2024-10-14 15:19:43 -0400

    Update Footer info and logo

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 
+import LogoIcon from 'components/icons/logo';
 import FooterMenu from 'components/layout/footer-menu';
 import LogoSquare from 'components/logo-square';
 import { getMenu } from 'lib/shopify';
@@ -9,7 +10,7 @@ const { COMPANY_NAME, SITE_NAME } = process.env;
 
 export default async function Footer() {
   const currentYear = new Date().getFullYear();
-  const copyrightDate = 2023 + (currentYear > 2023 ? `-${currentYear}` : '');
+  const copyrightDate = 2024 + (currentYear > 2024 ? `-${currentYear}` : '');
   const skeleton = 'w-full h-6 animate-pulse rounded bg-neutral-200 dark:bg-neutral-700';
   const menu = await getMenu('next-js-frontend-footer-menu');
   const copyrightName = COMPANY_NAME || SITE_NAME || '';
@@ -37,17 +38,18 @@ export default async function Footer() {
         >
           <FooterMenu menu={menu} />
         </Suspense>
-        {/* <div className="md:ml-auto">
+        <div className="md:ml-auto">
           <a
             className="flex h-8 w-max flex-none items-center justify-center rounded-md border border-neutral-200 bg-white text-xs text-black dark:border-neutral-700 dark:bg-black dark:text-white"
             aria-label="Deploy on Vercel"
-            href="https://vercel.com/templates/next.js/nextjs-commerce"
+            href="https://marks-bees.netlify.app"
           >
-            <span className="px-3">▲</span>
+            {/* <span className="px-3">▲</span> */}
+            <LogoIcon />
             <hr className="h-full border-r border-neutral-200 dark:border-neutral-700" />
-            <span className="px-3">Deploy</span>
+            <span className="px-3">Our Story</span>
           </a>
-        </div> */}
+        </div>
       </div>
       <div className="border-t border-neutral-200 py-6 text-sm dark:border-neutral-700">
         <div className="mx-auto flex w-full max-w-7xl flex-col items-center gap-1 px-4 md:flex-row md:gap-0 md:px-4 min-[1320px]:px-0">
@@ -55,7 +57,7 @@ export default async function Footer() {
             &copy; {copyrightDate} {copyrightName}
             {copyrightName.length && !copyrightName.endsWith('.') ? '.' : ''} All rights reserved.
           </p>
-          <hr className="mx-4 hidden h-4 w-[1px] border-l border-neutral-400 md:inline-block" />
+          {/* <hr className="mx-4 hidden h-4 w-[1px] border-l border-neutral-400 md:inline-block" /> */}
         </div>
       </div>
     </footer>
